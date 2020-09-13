a544cdb3da76244b6cf65412c4aaea232a6a9010
Sadman Sakib <ssakib5050@gmail.com>
2020-09-13 18:31:49 +0600

    Portfolio Icon Added

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
-import './Portfolio.css';
+import React from "react";
 
-class Portfolio extends React.Component{
-  render(){
-    return(
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-brands-svg-icons";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import "./Portfolio.css";
+
+class Portfolio extends React.Component {
+  render() {
+    return (
       <div className="portfolio">
         <h2 className="portfolio-header">Portfolios</h2>
 
         <div className="portfolio-items">
-
-          <PortfolioItem link="http://www.google.com" img="https://via.placeholder.com/480x360" title="Facebook v1 (Demo)"/>
-          <PortfolioItem link="http://www.facebook.com" img="https://via.placeholder.com/480x360" title="Facebook v2 (Demo)"/>
-          <PortfolioItem link="http://www.yahoo.com" img="https://via.placeholder.com/480x360" title="Project YH"/>
-          <PortfolioItem link="http://www.youtube.com" img="https://via.placeholder.com/480x360" title="Project YT"/>
-    
+          <PortfolioItem
+            link="http://www.google.com"
+            img="https://via.placeholder.com/480x360"
+            title="Facebook v1 UI (Demo)"
+          />
+          <PortfolioItem
+            link="http://www.facebook.com"
+            img="https://via.placeholder.com/480x360"
+            title="Facebook v2 UI (Demo)"
+          />
+          <PortfolioItem
+            link="http://www.yahoo.com"
+            img="https://via.placeholder.com/480x360"
+            title="Project YH"
+          />
+          <PortfolioItem
+            link="http://www.youtube.com"
+            img="https://via.placeholder.com/480x360"
+            title="Project YT"
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
-// External Function 
-function PortfolioItem(props){
-  const {link, img, title} = props;
-  return(
+// External Function
+function PortfolioItem(props) {
+  const { link, img, title } = props;
+  return (
     <div className="portfolio-item">
       <a href={link}>
-        <img src={img} alt=""/>
-        <h3 className='portfolio-title'>{title}</h3>
+        <img src={img} alt="" />
+        <h3 className="portfolio-title">{title}</h3>
+        <FontAwesomeIcon icon={faLink} className="protfolio__icon" />
       </a>
     </div>
-  )
+  );
 }
 
 export default Portfolio;
