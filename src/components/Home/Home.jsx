f7182d48a40acac0abf810ce3703a124cdd0fb37
Sadman Sakib <ssakib5050@gmail.com>
2020-11-13 21:46:20 +0600

    More Item Added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,43 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookF, faLinkedinIn, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faGithubAlt,
+} from "@fortawesome/free-brands-svg-icons";
 // import { faAddressBook } from '@fortawesome/free-solid-svg-icons'
-import './Home.css';
+import "./Home.css";
 
-
-class Home extends React.Component{
-  render(){
-    return(
+class Home extends React.Component {
+  render() {
+    return (
       <div className="container">
         <div>
-          <h1 className="home-title">My Name is <span className="home-title-name">Sadman Sakib</span></h1>
-          <h3 className="home-subtitle">I am a fullstack web developer(Frontend + Backend)</h3>
+          <h1 className="home-title">
+            My Name is <span className="home-title-name">Sadman Sakib</span>
+          </h1>
+          <h3 className="home-subtitle">
+            I am a fullstack web developer(Frontend + Backend)
+          </h3>
           <ul className="home-social-icons">
             <li>
-              <a href="http://localhost:3000">
+              <a href="http://www.facebook.com/sakib.5050" target="_blank">
                 <FontAwesomeIcon icon={faFacebookF} />
               </a>
             </li>
 
             <li>
-              <a href="http://localhost:3000">
+              <a href="https://www.linkedin.com/in/ssakib5050/" target="_blank">
                 <FontAwesomeIcon icon={faLinkedinIn} />
               </a>
             </li>
 
             <li>
-              <a href="http://localhost:3000">
+              <a href="https://github.com/ssakib5050" target="_blank">
                 <FontAwesomeIcon icon={faGithubAlt} />
               </a>
             </li>
           </ul>
         </div>
-
-        
-        
       </div>
-    )
+    );
   }
 }
 
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -14,7 +14,7 @@ class Portfolio extends React.Component {
         <div className="portfolio-items">
           <PortfolioItem
             link="https://facebook-react-firebase-2.web.app/login"
-            img="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/Screenshot.png?alt=media&token=e97bc5d9-e784-4900-b8dc-0875b09909cf"
+            img="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/Firefox_Screenshot_2020-11-13T13-00-20.241Z.png?alt=media&token=74597fde-befd-4a19-be35-6707cbb9c96e"
             title="✅Facebook v2 UI (Demo)"
           />
 
@@ -27,6 +27,23 @@ class Portfolio extends React.Component {
             link="https://instagram-react-firebase-a25ec.web.app/"
             img="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/Scresfsfenshot.png?alt=media&token=5aceb7a6-5b07-415b-be9d-be4940a6e295"
             title="✅Instagram UI (Demo)"
+          />
+          <PortfolioItem
+            link="https://twitter-react-firebase-c8ca2.web.app/"
+            img="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/Firefox_Screenshot_2020-11-13T13-49-03.911Z.png?alt=media&token=87ce1ba9-6fe5-4a52-9f73-c1ace7a1e0d9"
+            title="✅Twitter UI (Demo)"
+          />
+
+          <PortfolioItem
+            link="https://fbmessenger-react-firebase.web.app/"
+            img="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/Screenshot.png?alt=media&token=6cd1e9ce-3922-4fb9-b9a9-b6256ebf8b3b"
+            title="✅Messenger UI (Demo)"
+          />
+
+          <PortfolioItem
+            link="https://react-firebase-todo-25f3d.web.app/"
+            img="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/Firefox_Screenshot_2020-11-13T14-12-53.568Z.png?alt=media&token=f66a2fae-4daa-416c-8474-d5c1166c4205"
+            title="✅ToDo List (Demo)"
           />
         </div>
       </div>
