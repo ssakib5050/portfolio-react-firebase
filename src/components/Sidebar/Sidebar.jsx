4111648d7c379189b5313c4e360dd87433589a83
Sadman Sakib <ssakib5050@gmail.com>
2020-11-14 00:33:49 +0600

    Photo Upload Problem

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -26,7 +26,7 @@ class Sidebar extends React.Component {
             {/* Top Part  */}
             <div className="sidebar-header">
               <img
-                src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/22688473_484769155239918_4777493516804340989_n.jpg?alt=media&token=5dd16e30-98ee-4ded-a655-d76dd4982013"
+                src="https://firebasestorage.googleapis.com/v0/b/react-portfolio-92c60.appspot.com/o/22688473_484769155239918_4777493516804340989_n.jpg?alt=media&token=cfb66b86-51fa-43ff-989d-d255f2a3872d"
                 alt=""
                 className="sidebar-image"
               />
