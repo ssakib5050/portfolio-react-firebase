b54e228811f864e763bac39834a420f61dbb2005
Sadman Sakib <ssakib5050@gmail.com>
2021-04-10 05:37:16 +0600

    Home

--- src/App.js
+++ src/App.js
@@ -1,83 +1,95 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Sidebar from './components/Sidebar/Sidebar';
-import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
-import Portfolio from './components/Portfolio/Portfolio';
-import Resume from './components/Resume/Resume';
-import About from './components/About/About';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Switch, Route } from "react-router-dom";
 
-class App extends React.Component{
-  constructor(){
-    super();
+// import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./components/Home/Home";
+// import Contact from "./components/Contact/Contact";
+// import Portfolio from "./components/Portfolio/Portfolio";
+// import Resume from "./components/Resume/Resume";
+// import About from "./components/About/About";
 
-    this.state = {
-      sidebar: true
-    }
-  }
+import "./App.css";
 
-  sidebarToggle = () => {
-    this.setState({
-      sidebar: !this.state.sidebar
-    }) 
-  }
-
-  sidebarCheck = () => {
-    if(window.innerWidth > 786){
-      this.setState({sidebar: true});
-    }else{
-      this.setState({sidebar: false});
-    }
-  }
-
-  componentDidMount(){
-    this.sidebarCheck();
-
-    window.addEventListener('resize', () => {
-      this.sidebarCheck();
-    });
-  }
-
-  render(){
-    return (
-      <Router>
-  
-          <Sidebar sidebar={this.state.sidebar} sidebarToggle={this.sidebarToggle}/>
-          {/* <Home exact path="/home"/> */}
-  
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-  
-            <Route exact path="/contact">
-              <Contact />
-            </Route>
-  
-            <Route exact path="/portfolio">
-              <Portfolio/>
-            </Route>
-  
-            <Route exact path="/resume">
-              <Resume/>
-            </Route>
-  
-            <Route exact path="/about">
-              <About/>
-            </Route>
-  
-          </Switch>
-  
-      </Router>
-    )
-  }
-
-  
-}
+const App = () => {
+  // useEffect(() => {
+  //   window.addEventListener('resize', () => {
+  //     console.log('resizing');
+  //   });
+  // }, [])
+  return (
+    <div className="container-fluid dev">
+      <Home />
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends React.Component{
+//   constructor(){
+//     super();
+
+//     this.state = {
+//       sidebar: true
+//     }
+//   }
+
+//   sidebarToggle = () => {
+//     this.setState({
+//       sidebar: !this.state.sidebar
+//     })
+//   }
+
+//   sidebarCheck = () => {
+//     if(window.innerWidth > 786){
+//       this.setState({sidebar: true});
+//     }else{
+//       this.setState({sidebar: false});
+//     }
+//   }
+
+//   componentDidMount(){
+//     this.sidebarCheck();
+
+//     window.addEventListener('resize', () => {
+//       this.sidebarCheck();
+//     });
+//   }
+
+//   render(){
+//     return (
+//       <Router>
+
+//           <Sidebar sidebar={this.state.sidebar} sidebarToggle={this.sidebarToggle}/>
+//           {/* <Home exact path="/home"/> */}
+
+//           <Switch>
+//             <Route exact path="/">
+//               <Home />
+//             </Route>
+
+//             <Route exact path="/contact">
+//               <Contact />
+//             </Route>
+
+//             <Route exact path="/portfolio">
+//               <Portfolio/>
+//             </Route>
+
+//             <Route exact path="/resume">
+//               <Resume/>
+//             </Route>
+
+//             <Route exact path="/about">
+//               <About/>
+//             </Route>
+
+//           </Switch>
+
+//       </Router>
+//     )
+//   }
+
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// FontAwesomeIcon
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import {} from "@fortawesome/free-solid-svg-icons";
+library.add(fab);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
